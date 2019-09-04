8d0a656e27cd6d6ef47c861c1b92c06906bb0ad6
James Scalise <jamescscalise@gmail.com>
2019-09-04 15:59:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,2 +1,22 @@
 class Author
+  attr_accessor :name, :posts
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(song)
+    @posts << song
+    post.author=self
+  end
+  
+  def add_post_by_title(name)
+    post = Post.new(name)
+    post.author=self
+    @posts << post
+  end
+  
+  def self.post_count
+    Post.post_count
+  end
 end
